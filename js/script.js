08f8be53ffc26a64cdef0f14724a9c397c2d249e
Kunal Keshan <kunalkeshan12@gmail.com>
2021-10-14 07:15:47 +0530

    Player head music functionality resolved, it's working now

--- js/script.js
+++ js/script.js
@@ -34,6 +34,74 @@
 
 // });
 
+const {songs} = {
+    "songs": [
+        {
+            "name": "Eternal Youth",
+            "artist": "Rude",
+            "location": "./assets/Eternal.mp3",
+            "image": "./images/artist_image.jpeg",
+            "liked": true,
+            "id": 0
+        },
+        {
+            "name": "Never Gonna Give You Up",
+            "artist": "Rick Astely",
+            "location": "./assets/NGGYU.mp3",
+            "image": "./images/content/nggyp.jpg",
+            "liked": false,
+            "id": 1
+        },
+        {
+            "name": "Keeping It",
+            "artist": "Sou",
+            "location": "./assets/keepingIt.mp3",
+            "image": "./images/content/keepingIt.png",
+            "liked": false,
+            "id": 2
+        },
+        {
+            "name": "Eye of the Tiger",
+            "artist": "Surviour",
+            "location": "./assets/eyeTiger.mp3",
+            "image": "./images/content/survivor.jpg",
+            "liked": false,
+            "id": 3
+        },
+        {
+            "name": "Kenny G Collection",
+            "artist": "Kenny G",
+            "location": "./assets/kenny.mp3",
+            "image": "./images/content/kennyg.jpg",
+            "liked": false,
+            "id": 4
+        },
+        {
+            "name": "Noctornal",
+            "artist": "The Midnight",
+            "location": "./assets/noctornal.mp3",
+            "image": "./images/content/noctornal.jpg",
+            "liked": true,
+            "id": 5
+        },
+        {
+            "name": "Unravel",
+            "artist": "TK",
+            "location": "./assets/unravel.mp3",
+            "image": "./images/content/unravel.jpg",
+            "liked": false,
+            "id": 6
+        }
+    ]
+}
+
+let playBtn = document.getElementById("playBtn");
+let pauseBtn = document.getElementById("pauseBtn");
+const playerHead = document.getElementById("player");
+let currentSong = new Audio();
+
+playerHead.style.display = "none"
+
 const fetchData = async (url = "") => {
     try {
         const response = await fetch(url);
@@ -45,41 +113,97 @@ const fetchData = async (url = "") => {
 }
 
 const updatePlayer = ({name, artist, location, image, liked, id}) => {
+
+    playBtn = document.getElementById("playBtn");
+    pauseBtn = document.getElementById("pauseBtn");
+
+    currentSong.setAttribute("src", location);
+
     const songContainer = document.querySelector(".song");
     const artistContainer = document.querySelector(".artist");
-    const likeBtn = document.getElementById("like");
+    const likeBtn = document.querySelector(".likeBtn");
     const artistImage = document.querySelector(".artist_image");
-    const endTime = document.getElementById("end_time")
-    const song = new Audio(location);
-    var duration = null;
+    const endTime = document.getElementById("end_time");
+    
 
     songContainer.innerHTML = name;
     artistContainer.innerHTML = artist;
-    
-    likeBtn.style.color = "grey";
-    likeBtn.id = id
-    if(liked){
-        likeBtn.style.color = "red";
-    }
     artistImage.src = image;
 
-    song.onloadedmetadata = () => {
-        duration = song.duration;
+    currentSong.pause();
+    currentSong.currentTime = 0;
+
+    currentSong.onloadedmetadata = () => {
+        let duration = currentSong.duration;
         duration = (duration/60).toPrecision(3);
         endTime.innerHTML = duration + "";
     }
     
+    likeBtn.id = id;
+    likeBtn.style.color = "grey";
+    if(liked){
+        likeBtn.style.color = "red";
+    }
+    likeBtn.onclick = function () {
+        likeSong(likeBtn.id, likeBtn);
+    }    
+
+    playBtn.style.display = "inline";
+    pauseBtn.style.display = "none";
+
+    return currentSong;
+}
+
+const playPauseFunc = (song) => {
+    playBtn = document.getElementById("playBtn");
+    pauseBtn = document.getElementById("pauseBtn");
+    playBtn.addEventListener("click", () => {
+        song.play();
+        playBtn.style.display = "none";
+        pauseBtn.style.display = "inline";
+    });
+    pauseBtn.addEventListener("click", () => {
+        song.pause();
+        playBtn.style.display = "inline";
+        pauseBtn.style.display = "none";
+    });
+}
+
+const likeSong = (id, likeBtn) => {
+    const cardCollection = document.querySelectorAll(".card__collection_main");
+    const likedSongs = cardCollection[0].childNodes
+
+    songs.forEach((song, index) => {
+        if(song.id == id){
+            if(song.liked){
+                song.liked = false;
+                likeBtn.style.color = "grey";
+            } else {
+                song.liked = true;
+                likeBtn.style.color = "red";
+            }
+            likedSongs.forEach(songCard => {
+                if(songCard.hasChildNodes()){
+                    console.log(song.name)
+                    const name = songCard.lastChild.firstChild.innerHTML
+                    if(name != song.name){
+                        cardCollection[0].append(createCard(song));
+                    } else {
+                        songCard.style.display = "none";
+                        songCard.remove();
+                    }
+                }
+            });
+        }
+    })
+    // if(likeBtnColor === "red"){
+    //     likeBtn.style.color = "grey";
+    // } else {
+    //     likeBtn.style.color = "red";
+    // }
 }
 
 const createCard = (song) => {
-    // const card = 
-    // `<div class="card"> 
-    //     <img src="${song.image}" alt="${song.name}">  
-    //     <div class="card_info"> 
-    //         <p class="card_name">${song.name}</p> 
-    //         <p class="card_artist">${song.artist}</p> 
-    //     </div> 
-    // </div>`;
     const card = document.createElement("div");
     const img = document.createElement("img");
     const cardInfo = document.createElement("div");
@@ -100,19 +224,21 @@ const createCard = (song) => {
     card.append(img, cardInfo);
 
     card.onclick = function(){
-        updatePlayer(song)
+        playerHead.style.display = "flex";
+        currentSong = updatePlayer(song);
+        playPauseFunc(currentSong);
     }
 
     return card;
 }
 
-const updateCollection = (songs) => {
+const updateCollection = () => {
     const cardCollection = document.querySelectorAll(".card__collection_main");
     cardCollection.forEach((collection, index) => {
         if(index === 0){
             songs.forEach((song) => {
                 if(song.liked){
-                    collection.append(createCard(song));
+                    collection.append(createCard(song))
                 }
             })
         } else {
@@ -120,7 +246,6 @@ const updateCollection = (songs) => {
                 collection.append(createCard(song));
             });
         }
-
         if(index%2 !== 0){
             collection.classList.toggle("reverse")
         }
@@ -128,7 +253,8 @@ const updateCollection = (songs) => {
 }
 
 document.addEventListener("DOMContentLoaded", async() => {
-    const PATH = "./data/data.json";
-    const {songs} = await fetchData(PATH);
-    updateCollection(songs);
+    // const PATH = "./data/data.json";
+    // const {songs} = await fetchData(PATH);
+    updateCollection();
+
 })
