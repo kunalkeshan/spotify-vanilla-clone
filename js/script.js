a43e201738a5a4569c39b4b241c1106acdf126f4
Kunal Keshan <kunalkeshan12@gmail.com>
2021-10-16 00:11:07 +0530

    Comments added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+//An array of objects, where each object is a song. 
+// {songs} means destructuring the ojbect from the right.
+//Note: the destructing should be key specific. 
+//If songs is an key in the object, then only it will get the songs array. 
 const {songs} = {
     "songs": [
         {
@@ -59,74 +63,77 @@ const {songs} = {
     ]
 };
 
+// Getting required elemets, const to not allow reinitialization
+// let for reinitialization of that elements to update from DOM.
 const playerHead = document.getElementById("player");
 let playBtn = document.getElementById("playBtn");
 let pauseBtn = document.getElementById("pauseBtn");
 let cardCollection = document.querySelectorAll(".card__collection_main");
 let currentSong = new Audio();
 
-playerHead.style.display = "none"
+//Player is hidden by default and is visible only when a song is clicked.
+playerHead.style.display = "none";
 
-const fetchData = async (url = "") => {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.log(error);
+//Function to create card and add functionality to update player head.
+const createCard = (song) => {
+    //Takes in the song object as an argument.
+    /*
+    Create the Structure.
+    <div class="card">
+        <img src="./images/content/noctornal.jpg" alt="Noctornal">
+            <div class="card_info">
+                <p class="card_name">Noctornal</p>
+                <p class="card_artist">The Midnight</p>
+            </div>
+    </div>
+    */
+    const card = document.createElement("div");
+    const img = document.createElement("img");
+    const cardInfo = document.createElement("div");
+    const cardName = document.createElement("p");
+    const cardArtist = document.createElement("p");
+
+    //Assigning Classes to the elements created.
+    card.className = "card";
+    cardInfo.className = "card_info";
+    cardName.className = "card_name";
+    cardArtist.className = "card_artist";
+
+    //Adding the song details to the card.
+    cardName.innerHTML = song.name;
+    cardArtist.innerHTML = song.artist;
+    img.src = song.image;
+    img.alt = song.name;
+
+    //Structuring the card
+    cardInfo.append(cardName, cardArtist);
+    card.append(img, cardInfo);
+
+    //Adding functionality to the card to update the player head on click
+    card.onclick = function(){
+        playerHead.style.display = "flex";
+        currentSong = updatePlayer(song);
+        playPauseFunc(currentSong);
     }
+
+    //Return the dynamic card element.
+    return card;
 }
 
-const updatePlayer = ({name, artist, location, image, liked, id}) => {
-
-    playBtn = document.getElementById("playBtn");
-    pauseBtn = document.getElementById("pauseBtn");
-
-    currentSong.setAttribute("src", location);
-
-    const songContainer = document.querySelector(".song");
-    const artistContainer = document.querySelector(".artist");
-    const likeBtn = document.querySelector(".likeBtn");
-    const artistImage = document.querySelector(".artist_image");
-    const endTime = document.getElementById("end_time");
-    
-
-    songContainer.innerHTML = name;
-    artistContainer.innerHTML = artist;
-    artistImage.src = image;
-
-    currentSong.onloadedmetadata = () => {
-        let duration = currentSong.duration;
-        duration = (duration/60).toPrecision(3);
-        endTime.innerHTML = duration + "";
-    }
-    
-    likeBtn.id = id;
-    likeBtn.style.color = "grey";
-    if(liked){
-        likeBtn.style.color = "red";
-    }
-
-    likeBtn.onclick = function () {
-        likeSong(likeBtn.id, likeBtn, name);
-    }   
-    
-
-    playBtn.style.display = "inline";
-    pauseBtn.style.display = "none";
-
-    return currentSong;
-}
-
+//Adds functionality to the play and pause buttons to play the current song.
 const playPauseFunc = (song) => {
+    //Reinitialize the buttons.
     playBtn = document.getElementById("playBtn");
     pauseBtn = document.getElementById("pauseBtn");
     
+    //When the play button is clicked, the song is played.
     playBtn.addEventListener("click", () => {
         song.play();
         playBtn.style.display = "none";
         pauseBtn.style.display = "inline";
     });
+    
+    //When the pause button is clicked, the song is paused.
     pauseBtn.addEventListener("click", () => {
         song.pause();
         playBtn.style.display = "inline";
@@ -134,11 +141,26 @@ const playPauseFunc = (song) => {
     });
 }
 
+//Like and unlike the song and update the card collection for liked songs.
 const likeSong = (id, likeBtn, songName) => {
+    //id - the id of the song, on which the button is clicked for.
+    //likeBtn - The acutal element to update the styling.
+    //songName - The name of the song for which button is clicked
+    //           as we don't have id for every card, only the name is 
+    //           is unique, so we use it cross check the songs object.
+
+    //Reinitialize the collection to get the updated collection from DOM.
     cardCollection = document.querySelectorAll(".card__collection_main");
+    //Get the current liked songs from the above collection.
     let likedSongs = cardCollection[0].children;
+    //Since it returns a HTMLCollection, array functions can not be performed
+    //The likedSongs is converted into an array using the Array object
+    //converting it to an actual array using Array.from()l
     likedSongs = Array.from(likedSongs);
 
+    //Check if the global song object is liked or not
+    //if liked before, then unlike it, change color of like button
+    //and remove that song from the liked songs collection.
     if(songs[id].liked){
         songs[id].liked = false;
         likeBtn.style.color = "grey";
@@ -149,6 +171,9 @@ const likeSong = (id, likeBtn, songName) => {
                 songCard.remove();
             }
         });
+    //If song is not liked, then like the song,
+    //change the color of like button
+    //and add that song in the liked song collection.
     } else {
         songs[id].liked = true;
         likeBtn.style.color = "red";
@@ -156,55 +181,89 @@ const likeSong = (id, likeBtn, songName) => {
     }
 }
 
-const createCard = (song) => {
-    const card = document.createElement("div");
-    const img = document.createElement("img");
-    const cardInfo = document.createElement("div");
-    const cardName = document.createElement("p");
-    const cardArtist = document.createElement("p");
+//update the player head whenever a new song is clicked. 
+const updatePlayer = ({name, artist, location, image, liked, id}) => {
+    //The arugument of the function is a song object
+    //We are destructuing it in the arguments directly and using it.
 
-    card.className = "card";
-    cardInfo.className = "card_info";
-    cardName.className = "card_name";
-    cardArtist.className = "card_artist";
+    //Setting the new song for the global song object.
+    currentSong.setAttribute("src", location);
 
-    cardName.innerHTML = song.name;
-    cardArtist.innerHTML = song.artist;
-    img.src = song.image;
-    img.alt = song.name;
+    //Getting the required elements from the player head.
+    const songContainer = document.querySelector(".song");
+    const artistContainer = document.querySelector(".artist");
+    const likeBtn = document.querySelector(".likeBtn");
+    const artistImage = document.querySelector(".artist_image");
+    const endTime = document.getElementById("end_time");
 
-    cardInfo.append(cardName, cardArtist);
-    card.append(img, cardInfo);
+    playBtn = document.getElementById("playBtn");
+    pauseBtn = document.getElementById("pauseBtn");
 
-    card.onclick = function(event){
-        playerHead.style.display = "flex";
-        currentSong = updatePlayer(song);
-        playPauseFunc(currentSong);
+    //Setting the default to the player head pause and play buttons.
+    playBtn.style.display = "inline";
+    pauseBtn.style.display = "none";
+    
+    //Adding the selected song details in the player head.
+    songContainer.innerHTML = name;
+    artistContainer.innerHTML = artist;
+    artistImage.src = image;
+    
+    //Assign the id of the song to the button,
+    //Check is song is liked and add the color based on song.liked.
+    likeBtn.id = id;
+    likeBtn.style.color = "grey";
+    if(liked){
+        likeBtn.style.color = "red";
     }
 
-    return card;
+    //Adding a onclick functionlity to the like button.
+    likeBtn.onclick = function () {
+        likeSong(likeBtn.id, likeBtn, name);
+    }   
+
+    //When the current song is loaded, set it's duration and add it 
+    //to the end time element.
+    currentSong.onloadedmetadata = () => {
+        let duration = currentSong.duration;
+        duration = (duration/60).toPrecision(3) + "";
+        endTime.innerHTML = duration;
+    }
+
+    //Return the current song 
+    return currentSong;
 }
 
+//main function that calls all other functions.
+//updates the collection by creating the cards and adding them.
 const updateCollection = () => {
+    //Reinitalize the collection to get the latest from DOM.
     cardCollection = document.querySelectorAll(".card__collection_main");
+    //For all collections, using ForEach, we pass in the songs
+    //and create cards for each collection.
     cardCollection.forEach((collection, index) => {
+        //First collection is always for liked songs, so put
+        //liked songs alone in that collection.
         if(index === 0){
             songs.forEach((song) => {
                 if(song.liked){
                     collection.append(createCard(song))
                 }
             })
+        //all other collections put all songs.
         } else {
             songs.forEach((song) => {
                 collection.append(createCard(song));
             });
         }
+        //for every odd collection, reverse the order of the collection.
         if(index%2 !== 0){
             collection.classList.toggle("reverse")
         }
     })
 }
 
+//Once the document if fully loaded, call the update collection function
+//and add the functionlity to the Spotify Clone.
 document.addEventListener("DOMContentLoaded", async() => {
     updateCollection();
 })
